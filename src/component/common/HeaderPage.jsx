7e34db92c82c459c5872c842af8e50507693b095
haimomo24 <135099315+haimomo24@users.noreply.github.com>
2025-04-01 16:22:06 +0700

    push

--- src/component/common/HeaderPage.jsx
+++ src/component/common/HeaderPage.jsx
@@ -27,14 +27,9 @@ const HeaderPage = () => {
             title="Đăng nhập"
           >
             <FaSignInAlt className="text-lg text-[#800000]" />
-            <span className="ml-2 md:hidden">Đăng nhập</span>
+           
           </Link>
-          <Link
-            href="#"
-            className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
-          >
-            Get started
-          </Link>
+        
           <button
             data-collapse-toggle="mobile-menu-2"
             type="button"
@@ -42,7 +37,7 @@ const HeaderPage = () => {
             aria-controls="mobile-menu-2"
             aria-expanded="false"
           >
-            <span className="sr-only">Open main menu</span>
+           
           
             <svg
               className="hidden w-6 h-6"
